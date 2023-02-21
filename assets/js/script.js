515442e4760e0792103bbae1ca9eac9eec192360
adil-shabab <adilshabab2004@gmail.com>
2023-02-21 16:22:17 +0530

    home page completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,38 @@ $('.dep_carousel').slick({
         }
       ]
   });
+
+$('.doctor_carousel').slick({
+    dots: true,
+    infinite: true,
+    // autoplay:true,
+    autoPlayTimeout:2000,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: false,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
