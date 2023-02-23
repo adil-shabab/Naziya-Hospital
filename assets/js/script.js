44678201b15cf459300db7450a30a281c655df70
adil-shabab <adilshabab2004@gmail.com>
2023-02-23 16:37:25 +0530

    testimonial section added in about page

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,11 +43,52 @@ if(document.querySelector('.dep_carousel') !== null){
 // home page doctor carousel 
 if(document.querySelector('.doctor_carousel') !== null){
   $('.doctor_carousel').slick({
+    
     dots: true,
     infinite: true,
     // autoplay:true,
     autoPlayTimeout:2000,
     slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+}
+
+
+
+// home page doctor carousel 
+if(document.querySelector('.testimonial_carousel') !== null){
+  $('.testimonial_carousel').slick({
+    dots: true,
+    infinite: true,
+    // autoplay:true,
+    autoPlayTimeout:2000,
+    slidesToShow: 3,
     slidesToScroll: 2,
     arrows: false,
     responsive: [
